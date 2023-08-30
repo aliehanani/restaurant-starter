5252f15e1fae8afa22f799658dafa625ebd5190b
aliehanani <aliehanani@users.noreply.github.com>
2023-08-30 08:26:06 -0400

    Update OpenTableEmbed.vue

--- components/OpenTableEmbed.vue
+++ components/OpenTableEmbed.vue
@@ -2,7 +2,7 @@
 	<no-ssr class="bg-gray-100">
 		<script
 			type="text/javascript"
-			src="//www.opentable.com/widget/reservation/loader?rid=1041577&type=standard&theme=wide&iframe=true&overlay=false&domain=com&lang=en-US"
+			src="//www.opentable.com/"
 		></script>
 	</no-ssr>
 </template>
