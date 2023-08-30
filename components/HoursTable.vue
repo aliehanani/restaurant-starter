ab0c259e8a5f4175ab3d7634fad8796f582d5168
aliehanani <aliehanani@users.noreply.github.com>
2023-08-30 08:22:04 -0400

    Update HoursTable.vue
    matching Google Map hours

--- components/HoursTable.vue
+++ components/HoursTable.vue
@@ -1,22 +1,22 @@
 <template>
 	<dl class="flex flex-wrap items-center justify-between hours-operation">
 		<dt class="flex-shrink-0 uppercase font-semibold tracking-wider text-sm py-1">Monday</dt>
-		<dd class="flex-shrink-0 py-1">9:00 AM - 11:00 PM</dd>
+		<dd class="flex-shrink-0 py-1">12:00 PM - 11:00 AM</dd>
 		<span class="w-full"></span>
 		<dt class="uppercase font-semibold tracking-wider text-sm py-1">Tuesday</dt>
-		<dd class="py-1">9:00 AM - 11:00 PM</dd>
+		<dd class="py-1">12:00 PM - 12:00 AM</dd>
 		<span class="w-full"></span>
 		<dt class="uppercase font-semibold tracking-wider text-sm py-1">Wednesday</dt>
-		<dd class="py-1">9:00 AM - 11:00 PM</dd>
+		<dd class="py-1">12:30 PM - 12:00 AM</dd>
 		<span class="w-full"></span>
 		<dt class="uppercase font-semibold tracking-wider text-sm py-1">Thursday</dt>
-		<dd class="py-1">9:00 AM - 11:00 PM</dd>
+		<dd class="py-1">12:00 PM - 12:00 AM</dd>
 		<span class="w-full"></span>
 		<dt class="uppercase font-semibold tracking-wider text-sm py-1">Friday</dt>
-		<dd class="py-1">9:00 AM - 11:00 PM</dd>
+		<dd class="py-1">12:00 PM - 12:00 AM</dd>
 		<span class="w-full"></span>
 		<dt class="uppercase font-semibold tracking-wider text-sm py-1">Saturday</dt>
-		<dd class="py-1">9:00 AM - 11:00 PM</dd>
+		<dd class="py-1">12:00 PM - 12:00 AM</dd>
 		<span class="w-full"></span>
 	</dl>
 </template>
