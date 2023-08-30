906eb40d1e0e7216aeaddc320d7aee1b7caf260a
aliehanani <aliehanani@users.noreply.github.com>
2023-08-30 08:25:01 -0400

    Update OpenTableButton.vue

--- components/OpenTableButton.vue
+++ components/OpenTableButton.vue
@@ -1,6 +1,6 @@
 <template>
 	<a
-		href="https://www.opentable.com/r/south-village-grille-reservations-columbus?restref=1041577&lang=en-US"
+		href="https://www.opentable.com"
 		target="_blank"
 		rel="noopener noreferrer"
 		class="bg-brand text-brand_contrast py-2 px-3 leading-none font-bold"
